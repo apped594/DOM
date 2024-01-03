bd54f2f2185b2d02231850d870d8d93422c3afb7
Jonathan <jonathan@d730.co.za>
2024-01-03 19:27:15 +0200

    project1 done

--- Project1/script.js
+++ Project1/script.js
@@ -1,1 +1,39 @@
-console.log('test');
+const vloeker = document.querySelector('.wie-my-gevloek');
+const vloek = document.querySelector('.fokit');
+const vloekWeer = document.querySelector('.vloek-my-weer');
+const vloekArr = [
+    
+    {
+        vloekWoord: "Jou dik tril",
+        persoon: "Pielie Lang arm"
+
+    },
+
+    {
+        vloekWoord: "Jou stink lip man",
+        persoon: "Mikayla stink tiete"
+
+    },
+
+    {
+        vloekWoord: "Jou oupa se nat wielsnare",
+        persoon: "Lang tietie dam tietie"
+
+    },
+
+    {
+        vloekWoord: "Jou Moot Hond",
+        persoon: "Moot Hond"
+
+    }
+
+]
+
+vloekWeer.addEventListener("click", e =>{
+
+    let random = Math.floor(Math.random() * vloekArr.length);
+
+    vloeker.innerText = vloekArr[random].persoon;
+    vloek.innerText = vloekArr[random].vloekWoord
+});
+
